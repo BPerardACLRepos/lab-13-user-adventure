089265787272091c5b0e22aa4f5cc1878da6bbe8
Brandon Perard <brandon.perard@gmail>
2021-01-27 23:55:10 -0800

    add attempt at trivia api as data source

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-// import functions and grab DOM elements
+// const questions = [];
 
-// initialize state
 
-// set event listeners to update state and DOM
+// fetch('https://opentdb.com/api.php?amount=10').then(response => response.json()).then(data => {
+//     console.log(data);
+//     questions.push(...data.results);
+// });
+
+// for (let item of questions) {
+//     console.log(item);
+// }
+
+async function asyncCall() {
+    let url = 'https://opentdb.com/api.php?amount=10';
+    let response = await fetch(url);
+
+
+
+    let test = await response.json();
+    localStorage.setItem('questions', JSON.stringify(test.results));
+}
+
+const stringQuests = localStorage.getItem('questions');
+const questions = JSON.parse(stringQuests);
+
+for (let question of questions) {
+    let fixed = question.question;
+    console.log(fixed);
+}
