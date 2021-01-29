c947a793fa77572e88dbba0888eb85edf77c0055
Brandon Perard <brandon.perard@gmail>
2021-01-29 11:42:55 -0800

    update user outcomes with img and user stats

--- results/results.js
+++ results/results.js
@@ -9,21 +9,25 @@ const user = getStorageItem(USER);
 if (user.lives === 0) {
     outcomeTitle.textContent = `Maybe Next Time...`;
     const p = document.createElement('p');
-    p.textContent = `The good news is, it's just trivia. The bad news is, the whole point of this was trivia. I guess that's kind of the same thing, sorry I didn't come up with something more helpful.`;
+    p.textContent = `${user.name} the good news is, it's just trivia. The bad news is, the whole point of this was trivia. I guess that's kind of the same thing, sorry I didn't come up with something more helpful.`;
     const img = document.createElement('img');
     img.src = '../assets/try-again.gif';
     outcomeSummary.append(p, img);
 } else if (user.completedQuests === parseInt(user.difficulty)) {
     outcomeTitle.textContent = `Welcome Quizzard!`;
     const p = document.createElement('p');
-    p.textContent = `First off, I never doubted you. Secondly, I didn't think anybody would actually see this message, so...`;
+    p.textContent = `First off ${user.name}, I never doubted you. Secondly, I didn't think anybody would actually see this message, so...`;
     const img = document.createElement('img');
     img.src = '../assets/celebrate.gif';
     outcomeSummary.append(p, img);
 } else {
     outcomeTitle.textContent = `You Tried.`;
     const p = document.createElement('p');
-    p.textContent = `You kind of gave it a good go, so that's something.`;
+    p.textContent = `You kind of gave it a good go ${user.name}, so that's something.`;
     const img = document.createElement('img');
     img.src = '../assets/meh.gif';
+    outcomeSummary.append(p, img);
 }
+const p = document.createElement('p');
+p.textContent = `FINAL STATS: Completed Quests ${user.completedQuests} - Correct Answers ${user.correctAnswers} - Remaining Lives ${user.lives} - Remaining Skips ${user.skips}`;
+outcomeSummary.append(p);
